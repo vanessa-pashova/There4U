15008328f4bb35a3a6dd2034b17ab63ac1f5a753
vanessa-pashova <vanesapashova16@gmail.com>
2025-02-16 16:11:17 +0200

    Methods added

--- src/main/java/com/example/there4u/repositories/UserRepository.java
+++ src/main/java/com/example/there4u/repositories/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsById(Long id);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsPhoneNumber(String phoneNumber);
 }
