4190fc66ced3e962a27b951b8f6b34a134315682
vanessa-pashova <vanesapashova16@gmail.com>
2025-04-22 13:16:25 +0300

    Repository Creation

---
+++ src/main/java/com/example/there4u/There4UApplication.java
@@ -0,0 +1,13 @@
+package com.example.there4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class There4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(There4UApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/there4u/There4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.there4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class There4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
