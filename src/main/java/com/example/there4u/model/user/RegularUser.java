dbbab25df2785f29bf25e29a2ab42b4ed75e77b4
diqn <didkot0334@gmail.com>
2025-04-26 16:01:20 +0300

    Added column names, and set the TypeOfUser field to be REGULAR_USER in the args constructor.

--- src/main/java/com/example/there4u/model/user/RegularUser.java
+++ src/main/java/com/example/there4u/model/user/RegularUser.java
@@ -1,15 +1,23 @@
 package com.example.there4u.model.user;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
+@Table(name = "regular_user")
 public class RegularUser extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private long id;
+
     @Pattern(regexp = "^[0-9]{10}", message = "!> Incorect Civil Number. Civil Number must be exactly 10 digits")
+    @Column(name = "UCN")
     private String UCN; //Unique Civil Number
 
-    public RegularUser(String username, String name, String email, String password, String phoneNumber, String address, TypeOfUser typeOfUser, String UCN ) {
-        super(username, name, email, password, phoneNumber, address, typeOfUser);
+    public RegularUser(String username, String name, String email, String password, String phoneNumber, String address,  String UCN ) {
+        super(username, name, email, password, phoneNumber, address, TypeOfUser.REGULAR_USER);
         this.UCN = UCN;
     }
 
