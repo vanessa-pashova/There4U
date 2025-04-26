5642ad4bcf5f4be3db25e439b89c309af78ec17f
diqn <didkot0334@gmail.com>
2025-04-26 14:54:48 +0300

    Added annotations for UCN field and adjusted args constructor for RegularUser class.

--- src/main/java/com/example/there4u/model/user/RegularUser.java
+++ src/main/java/com/example/there4u/model/user/RegularUser.java
@@ -1,14 +1,16 @@
 package com.example.there4u.model.user;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class RegularUser extends User {
+    @Pattern(regexp = "^[0-9]{10}", message = "!> Incorect Civil Number. Civil Number must be exactly 10 digits")
     private String UCN; //Unique Civil Number
 
-    public RegularUser(String name, String email, String password, String phoneNumber, String address, TypeOfUser typeOfUser, String UCN ) {
-        super(name, email, password, phoneNumber, address, typeOfUser);
-        this.setUCN(UCN);
+    public RegularUser(String username, String name, String email, String password, String phoneNumber, String address, TypeOfUser typeOfUser, String UCN ) {
+        super(username, name, email, password, phoneNumber, address, typeOfUser);
+        this.UCN = UCN;
     }
 
     public void setUCN(String UCN) {
