b40a47d06c143d064897b5b8130c9017d3197110
diqn <didkot0334@gmail.com>
2025-04-26 14:50:36 +0300

    Adjusted NGOUser class constructor, and annotated its NGOid field.

--- src/main/java/com/example/there4u/model/user/NGOUser.java
+++ src/main/java/com/example/there4u/model/user/NGOUser.java
@@ -1,16 +1,21 @@
 package com.example.there4u.model.user;
 
 import com.example.there4u.model.validators.NGOidValidator;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 @Getter
 public class NGOUser extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long NGOid;
+
     private String description;
 
-    public NGOUser(String name, String email, String password, String phoneNumber, String address, TypeOfUser typeOfUser, long NGOid, String description) {
-        super(name, email, password, phoneNumber, address, typeOfUser);
-        this.setNGOid(NGOid);
+    public NGOUser(String username, String name, String email, String password, String phoneNumber, String address, TypeOfUser typeOfUser, String description) {
+        super(username, name, email, password, phoneNumber, address, typeOfUser);
         this.setDescription(description);
     }
 
