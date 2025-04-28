ca6c9ae0302a1e04d07cb3286bd2e8a24af81800
vanessa-pashova <vanesapashova16@gmail.com>
2025-04-28 16:10:53 +0300

    Contributor Class Added

--- src/main/java/com/example/there4u/model/contributor/Contributor.java
+++ src/main/java/com/example/there4u/model/contributor/Contributor.java
@@ -1,59 +1,80 @@
 package com.example.there4u.model.contributor;
 
-import com.example.there4u.model.validators.*;
+import com.example.there4u.model.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-public class Contributor {
-    @Setter
+@Setter
+public class Contributor extends User {
     TypeOfContributor typeOfContributor;
-    private String name;
-    private String email;
-    private String password;
-    private String phoneNumber;
-    private long contributorID;
-    private String description;
 
-    public Contributor(TypeOfContributor typeOfContributor, String name, String email, String password, String phoneNumber, long contributorID, String description) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ContributorID")
+    @NotNull(message = ">! ContributorID cannot be null")
+    @Pattern(regexp = "^(91|92|93)[0-9]{3}$", message = "Contributor ID must start with 91, 92 or 93 and have exactly 5 digits.")
+    private String ContributorID;
+
+    @Column(name = "description")
+    private String description = "[No description added]";
+
+    private boolean isValidTypeOfContributor(TypeOfContributor typeOfContributor, String ContributorID) {
+        int code = Integer.parseInt(ContributorID.substring(0, 2));
+        switch(code) {
+            case 91: {
+                if(typeOfContributor == TypeOfContributor.CANTEEN) {
+                    return true;
+                }
+
+                break;
+            }
+
+            case 92: {
+                if(typeOfContributor == TypeOfContributor.GROCERY_STORE) {
+                    return true;
+                }
+
+                break;
+            }
+
+            case 93: {
+                if(typeOfContributor == TypeOfContributor.RESTAURANT) {
+                    return true;
+                }
+
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    private void checkContributorIdAndType(TypeOfContributor typeOfContributor, String ContributorID) {
+        if(!isValidTypeOfContributor(typeOfContributor, ContributorID)) {
+            throw new IllegalArgumentException("Contributor ID is not valid or invalid type of contributor");
+        }
+    }
+
+    public Contributor(String username, String name, String email, String password, String phoneNumber, String address, String contributorID, String typeOfContributor, String description) {
+        super(username, name, email, password, phoneNumber, address);
         this.setTypeOfContributor(typeOfContributor);
-        this.setName(name);
-        this.setEmail(email);
-        this.setPassword(password);
-        this.setPhoneNumber(phoneNumber);
-        this.setContributorID(contributorID);
+        this.ContributorID = contributorID;
+        checkContributorIdAndType(this.typeOfContributor, this.ContributorID);
         this.setDescription(description);
     }
 
-    public void setName(String name){
-        this.name = NameValidator.validateName(name);
-    }
-
-    public void setEmail(String email){
-        if(!EmailValidator.isValidEmail(email)){
-            throw new IllegalArgumentException(">! Invalid email address [setEmail(), Contributor]");
-        }
-
-        this.email = email;
-    }
-
-    public void setPassword(String password){
-        if(PasswordValidator.validatePassword(password)) {
-            this.password = password;
-        }
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        if(!PhoneNumberValidator.isValidPhoneNumber(phoneNumber)) {
-            throw new IllegalArgumentException(">! Invalid phone number [setPhoneNumber(), Contributor]");
-        }
-
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setContributorID(long contributorID){
-        if(ContributorIdValidator.isValidContributorId(contributorID)) {
-            this.contributorID = contributorID;
+    public void setTypeOfContributor(String typeOfContributor) {
+        switch (typeOfContributor.toUpperCase()) {
+            case "CANTEEN" -> this.typeOfContributor = TypeOfContributor.CANTEEN;
+            case "RESTAURANT" -> this.typeOfContributor = TypeOfContributor.RESTAURANT;
+            case "GROCERY_STORE" -> this.typeOfContributor = TypeOfContributor.GROCERY_STORE;
         }
     }
 
