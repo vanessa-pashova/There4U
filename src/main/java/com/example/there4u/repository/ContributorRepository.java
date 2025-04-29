f3ec290b0901b8eeb7bd06f1d788de5ed9b5c3cf
vanessa-pashova <vanesapashova16@gmail.com>
2025-04-29 15:48:29 +0300

    Contributor Repository added

---
+++ src/main/java/com/example/there4u/repository/ContributorRepository.java
@@ -0,0 +1,7 @@
+package com.example.there4u.repository;
+
+import com.example.there4u.model.contributor.Contributor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContributorRepository extends JpaRepository<Contributor, String> {
+}
