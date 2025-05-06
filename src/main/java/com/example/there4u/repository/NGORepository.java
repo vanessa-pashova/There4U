17ffcc241c398df6db9f8d662b51743bb63e2757
vanessa-pashova <vanesapashova16@gmail.com>
2025-05-07 00:02:58 +0300

    Code reformatting

--- src/main/java/com/example/there4u/repository/NGORepository.java
+++ src/main/java/com/example/there4u/repository/NGORepository.java
@@ -3,6 +3,6 @@ package com.example.there4u.repository;
 import com.example.there4u.model.user.NGOUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NGORepository extends JpaRepository<NGOUser, String> {
+public interface NGORepository extends JpaRepository<NGOUser, Long> {
 
 }
