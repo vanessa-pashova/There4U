fd5c58e4a38f994faa357fee50bc2b5e5d0a9547
vanessa-pashova <vanesapashova16@gmail.com>
2025-05-07 15:12:23 +0300

    Image Repository created

---
+++ src/main/java/com/example/there4u/repository/badge/BadgeRepository.java
@@ -0,0 +1,7 @@
+package com.example.there4u.repository.badge;
+
+import com.example.there4u.model.badge.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
+}
