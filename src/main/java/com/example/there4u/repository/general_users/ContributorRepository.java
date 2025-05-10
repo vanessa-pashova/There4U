0da6aa1546196161459ff038ec5f0506d28c1e86
diqn <didkot0334@gmail.com>
2025-05-10 22:15:15 +0300

    Fixed type of primary key.

--- src/main/java/com/example/there4u/repository/general_users/ContributorRepository.java
+++ src/main/java/com/example/there4u/repository/general_users/ContributorRepository.java
@@ -3,7 +3,7 @@ package com.example.there4u.repository.general_users;
 import com.example.there4u.model.user.Contributor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContributorRepository extends JpaRepository<Contributor, String> {
+public interface ContributorRepository extends JpaRepository<Contributor, Long> {
     Contributor findContributorById(long id);
     Contributor findContributorByUsername(String username);
     Contributor findContributorByEmail(String email);
