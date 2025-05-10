53a90d14fa0d23fd0483a65e7128f46ecf456fb3
diqn <didkot0334@gmail.com>
2025-05-10 22:34:46 +0300

    Added a method to snatch all publications from a particular user.

--- src/main/java/com/example/there4u/repository/publication/PublicationRepository.java
+++ src/main/java/com/example/there4u/repository/publication/PublicationRepository.java
@@ -3,5 +3,8 @@ package com.example.there4u.repository.publication;
 import com.example.there4u.model.publication.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PublicationRepository extends JpaRepository<Publication, Long> {
+    List<Publication> findByOwnerId(Long ownerId);
 }
