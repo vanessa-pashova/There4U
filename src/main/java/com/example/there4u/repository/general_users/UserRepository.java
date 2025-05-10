1e6d6ab8c361d015797829fead6e36329c39c2f9
diqn <didkot0334@gmail.com>
2025-05-10 22:18:32 +0300

    Added UserRepository.

---
+++ src/main/java/com/example/there4u/repository/general_users/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.there4u.repository.general_users;
+
+import com.example.there4u.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
