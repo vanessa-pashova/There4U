75779ababbc780c7d19246609421c119652e79ad
diqn <didkot0334@gmail.com>
2025-05-10 22:16:09 +0300

    ImageRepository now extending JpaRepository instead of CrudRepository.

--- src/main/java/com/example/there4u/repository/image/ImageRepository.java
+++ src/main/java/com/example/there4u/repository/image/ImageRepository.java
@@ -1,7 +1,7 @@
 package com.example.there4u.repository.image;
 
 import com.example.there4u.model.image.Image;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends CrudRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
