bf469b76b26e99a7aee28a48b60042b2cd3563b3
diqn <didkot0334@gmail.com>
2025-05-10 22:19:57 +0300

    Added PublicationService. Implemented publish method for adding publications to the database.

---
+++ src/main/java/com/example/there4u/service/publication/PublicationService.java
@@ -0,0 +1,35 @@
+package com.example.there4u.service.publication;
+
+import com.example.there4u.dto.publication.PublicationRequestDto;
+import com.example.there4u.dto.publication.PublicationResponseDto;
+import com.example.there4u.model.publication.Publication;
+import com.example.there4u.model.user.User;
+import com.example.there4u.repository.general_users.UserRepository;
+import com.example.there4u.repository.publication.PublicationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PublicationService {
+    private final PublicationRepository publicationRepository;
+    private final UserRepository userRepository;
+
+    public PublicationService(PublicationRepository publicationRepository, UserRepository userRepository) {
+        this.publicationRepository = publicationRepository;
+        this.userRepository = userRepository;
+    }
+
+    public PublicationResponseDto publish(PublicationRequestDto publicationRequest, Long ownerId) {
+        User owner = userRepository.findById(ownerId).
+                orElseThrow(() -> new EntityNotFoundException("Could not find user with id " + ownerId));
+
+        Publication publication = new Publication(publicationRequest);
+        publication.setOwner(owner);
+
+        Publication savedPublication = publicationRepository.save(publication);
+
+        return new PublicationResponseDto(savedPublication);
+    }
+}
