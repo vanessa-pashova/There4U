92daa34c42c2fc87619b51e3f0555ef0af1c6f8c
diqn <didkot0334@gmail.com>
2025-05-19 12:47:51 +0300

    Edited LoginResponse to accept a JWT string token and the expiration time.

--- src/main/java/com/example/there4u/dto/login/LoginResponse.java
+++ src/main/java/com/example/there4u/dto/login/LoginResponse.java
@@ -3,13 +3,5 @@ package com.example.there4u.dto.login;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record LoginResponse(
-        @NotBlank
-        String userType,
-
-        @NotNull
-        Long userId,
-
-        @NotBlank
-        String username
-) {}
+public record LoginResponse(String token, long expiresIn) {
+}
