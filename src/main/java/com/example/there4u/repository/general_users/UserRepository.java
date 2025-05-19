efe6c8fa553fa20035f62fc0075dc67cccf36c1c
vanessa-pashova <vanesapashova16@gmail.com>
2025-05-19 14:16:47 +0300

    One method added

--- src/main/java/com/example/there4u/repository/general_users/UserRepository.java
+++ src/main/java/com/example/there4u/repository/general_users/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.there4u.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(long id);
 }
